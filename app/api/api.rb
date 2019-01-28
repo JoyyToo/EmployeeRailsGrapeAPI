1aebc24cc06572c807fbfd16467a87a7b49a2e84
joyytoo <joytoo97@gmail.com>
2019-01-28 12:15:10 +0300

    Add get all and create employee

---
+++ app/api/api.rb
@@ -0,0 +1,29 @@
+class API < Grape::API
+    prefix 'api'
+    version 'v1', using: :path
+    format :json
+
+    resource :employee_data do
+        desc "List all Employee"
+  
+        get do
+          Employee.all
+        end
+
+        desc "create a new employee"
+        ## This takes care of parameter validation
+        params do
+            requires :name, type: String
+            requires :address, type:String
+            requires :age, type:Integer
+        end
+        ## This takes care of creating employee
+        post do
+        Employee.create!({
+            name:params[:name],
+            address:params[:address],
+            age:params[:age]
+        })
+        end
+    end
+end
